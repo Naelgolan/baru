c41f5e53c33e3027d9a3a027fe92c6b1f9d59d8f
unknown <naelgolan1223@gmail.com>
2026-05-26 17:17:54 +0700

    bersihkan folder duplikat -main dari repository

--- be-mindease-main/be-mindease-main/routes/postRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const postController = require('../controllers/postController');
-const { authenticateToken } = require('../middleware/authMiddleware');
-
-router.get('/', postController.getPosts);
-router.post('/', authenticateToken, postController.createPost);
-
-module.exports = router;
--- be-mindease-main/be-mindease-main/routes/publicRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const publicController = require('../controllers/publicController');
-
-router.get('/settings', publicController.getSettings);
-router.get('/doctors', publicController.getDoctors);
-
-module.exports = router;
